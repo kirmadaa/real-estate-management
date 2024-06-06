eac027cd9ca27001f614fb549c595d41eb97f94c
Himanshu Tripathi <100154262+kirmadaa@users.noreply.github.com>
2024-06-06 23:24:48 +0530

    Update environment.prod.ts

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   api: {
-    server: 'http://localhost:3000/',
+    server: 'http://13.233.104.2:3000/',
     mapKey: 'AAPK813f477568b44879a2f13ed12f45d5aeqrUonQPMhXY5H_rXFhCH97UBYzhi2rsy_oAeI1orfCfBAS1BaUxtnSM1dXQuThCM',
     googleAuthClientId: '',
-    webSocketUrl: ''
+    webSocketUrl: 'ws://13.233.104.2:3000/websocket'
   }
 };
